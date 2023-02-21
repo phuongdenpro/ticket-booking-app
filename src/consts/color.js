ff962df45ec44a07582f06e02e665837dbfdd600
phuong <phuongdenpro@gmail.com>
2023-02-21 14:40:11 +0700

    setup

---
+++ src/consts/color.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    white: '#FFF',
+    dark: '#000',
+    primary: '#F9813A',
+    secondary: '#fedac5',
+    light: '#E5E5E5',
+    grey: '#908e8c',
+    red: '#F52A2A',
+    green: '#00B761',
+  };
+  
+  export default COLORS;
